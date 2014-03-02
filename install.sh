c195bfbbc95ae0a67de618c8ec4521f0a8d145a2
Max Edmands <demands@heinlein.raftercat>
2014-03-02 12:20:31 -0800

    Install homebrew if not already there

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if ! which -s brew; then
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
 if [ -d $HOME/Projects/dotfiles ] && ! [ -L $HOME/.dotfiles ]; then
   ln -s $HOME/Projects/dotfiles $HOME/.dotfiles
 fi
