d347f4d00fadd5547c064d66b07d3b489e32e402
Max Edmands <git@maxedmands.com>
2015-05-31 17:19:15 -0700

    osx defaults that I can really get behind
    - got rid of settings that throw errors
    - add solarized syntax coloring to iTerm and Terminal.app

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+# download homebrew
 if ! which -s brew; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# create dotfiles directory
 if [ -d $HOME/Projects/dotfiles ] && ! [ -L $HOME/.dotfiles ]; then
   ln -s $HOME/Projects/dotfiles $HOME/.dotfiles
 fi
 
+# run pre-up
 source $HOME/.dotfiles/hooks/pre-up
 
+# osx defaults
 echo "Setting up osx defaults..."
 source $HOME/.dotfiles/hooks/osx_defaults
 
+# finally, run rcup
 RCRC=$HOME/.dotfiles/rcrc rcup
